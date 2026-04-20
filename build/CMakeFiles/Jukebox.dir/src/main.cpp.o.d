13a87d3c9f296da92b7323b341ff72a3c86bb1d9
tobias-c <tump.linux@gmail.com>
2026-04-20 17:51:20 +0100

    Removed build from tracking

--- build/CMakeFiles/Jukebox.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Jukebox.dir/src/main.cpp.o: \
- /home/tobias/dev/Jukebox/src/main.cpp /usr/include/stdc-predef.h \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
- /home/tobias/dev/Jukebox/include/Constants.hpp
